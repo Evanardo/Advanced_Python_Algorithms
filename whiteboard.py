7b3b81aa778fc911be548f4b4619f37413ca4ab2
Evan Lewis <monty.lewis1@outlook.com>
2022-10-05 17:16:54 -0500

    damn it

---
+++ whiteboard.py
@@ -0,0 +1,39 @@
+# Consecutive Indices
+# You are given a list of unique integers (arr), and two integers (a and b). \
+#     Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+# It is guaranteed that a and b are both present in arr.
+# Example:
+# Input: ([1, 6, 9, -3, 4, -78, 0], -3, 4)				
+# Output: True
+# Input: ([3,1,0,19], 19, 0)	
+# Output: True
+
+def arr(a,b):
+    a= []
+    b= []
+    for i in a:
+        if a == b:
+            return True
+        else:
+            return False
+
+arr()
+
+# def arr1(*numbers):
+#     print(arr1)
+
+
+# arr1(1,2,3,5,6,3,7,8)
+
+# def arr2(*numbers):
+#     print(arr2)
+
+
+# arr2(1,2,3,5,6,4,7,8)
+
+# def result(ans):
+#     for i in arr1:
+#         if arr1 == arr2:
+#             return True
+#         else:
+#             return False
